4560210baf19a7762bfa48e4e8e0e01209d129c6
kavyas127 <officialkesai315@gmail.com>
2024-05-29 14:37:14 +0530

    npm installed and home route built

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+const ejs = require("ejs");
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+// Routes
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000");
+});
